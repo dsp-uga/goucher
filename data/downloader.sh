a774bcbc01f72a61c1edd0c7d23f3c6350e83ded
Omid <os14526@uga.edu>
2018-03-23 13:45:01 -0400

    adds downloader for test and training set fixes #1

--- data/downloader.sh
+++ data/downloader.sh
@@ -10,4 +10,35 @@ then
     wget "https://storage.googleapis.com/uga-dsp/project4/train.txt"
 fi
 
+# create folders
+if [ ! -d "train" ]
+then
+    mkdir "train"
+fi
 
+if [ ! -d "test" ]
+then
+    mkdir "test"
+fi
+
+echo "staring to download training"
+cd test
+
+while read -r line || [[ -n "$line" ]]; do
+    echo "$line"
+    wget "https://storage.googleapis.com/uga-dsp/project4/data/$line.tar"
+    tar -xf  "$line.tar"
+    rm "$line.tar"
+done < "../test.txt"
+
+
+echo "downloading train"
+
+cd ../train
+
+while read -r line || [[ -n "$line" ]]; do
+    echo "$line"
+    wget "https://storage.googleapis.com/uga-dsp/project4/data/$line.tar"
+    tar -xf  "$line.tar"
+    rm "$line.tar"
+done < "../train.txt"
