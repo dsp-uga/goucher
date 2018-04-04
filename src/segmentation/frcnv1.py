77fce998ffb0191f82c90600fa6993b6f893cd04
Hiten Mahesh Nirmal <hitennirmal94@gmail.com>
2018-04-04 08:22:44 -0400

    Trying FRCN
    Version 1. Network not running completely

---
+++ src/segmentation/frcnv1.py
@@ -0,0 +1,195 @@
+
+
+import logging
+import numpy as np
+import os
+import tensorflow as tf
+from keras.models import Model
+
+from keras.layers import Input, merge, concatenate, Conv2D, MaxPooling2D, Activation, UpSampling2D, Dropout, Conv2DTranspose, UpSampling2D, Lambda
+from keras.layers.normalization import BatchNormalization as bn
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.optimizers import RMSprop
+from keras import regularizers
+from keras import backend as K
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.layers.merge import add
+import numpy as np
+from keras.regularizers import l2
+import cv2
+import glob
+import h5py
+from keras.models import load_model
+import os
+
+from src.segmentation.segmenter import  Segmenter
+
+
+class FRCN_Segmenter(Segmenter):
+    """
+    This class provides the implementation for the UNet classifier
+    """
+
+    def __init__(self, loss_function="dice_coef"):
+        self.trained_model = None
+        self.classifier_name = "UNET"
+        self.data = None
+
+        if (loss_function == "dice_coef"):
+            self.metrics_function = UNET_Segmenter.dice_coef
+            self.loss_function = UNET_Segmenter.dice_coef_loss
+        elif loss_function == "dice_coef2":
+            self.metrics_function = UNET_Segmenter.dice_coef2
+            self.loss_function = UNET_Segmenter.dice_coef_loss2
+
+    def dice_coef2(y_true, y_pred):
+        """
+        this is a modified version of dice score,
+        :param y_true: ground truth
+        :param y_pred: predicted
+        :return: dice score calculated betweenthe actual and predicted versions
+        """
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+
+        intersection = K.sum(y_true_f * y_pred_f)
+        U = K.sum(y_true_f * y_pred_f)
+
+        return 1 - intersection / U
+
+    def dice_coef(y_true, y_pred):
+        """
+        This is dice score implemetation
+        :param y_true: ground truth
+        :param y_pred: predicted
+        :return: dice score calculated betweenthe actual and predicted versions
+        """
+        smooth = 1.0
+        y_true_f = K.flatten(y_true)
+        y_pred_f = K.flatten(y_pred)
+
+        print(K.max(y_true))
+
+        intersection = K.sum(y_true_f * y_pred_f)
+        return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+    def dice_coef_loss(y_true, y_pred):
+        return - UNET_Segmenter.dice_coef(y_true, y_pred)
+
+    def dice_coef_loss2(y_true, y_pred):
+        return - UNET_Segmenter.dice_coef(y_true, y_pred)
+
+    def load_model(self, import_path):
+        """
+        overrides the load method to add the costum object
+        :param import_path: directory from which model has to be loaded
+        """
+        self.trained_model = load_model(os.path.join(import_path, self.classifier_name + ".h5"),
+                                        custom_objects={
+                                                        'dice_coef_loss': UNET_Segmenter.dice_coef_loss,
+                                                        'dice_coef': UNET_Segmenter.dice_coef})
+        logging.info("Loaded Model at : " + os.path.join(import_path, self.classifier_name + ".h5"))
+
+    def train(self, x_train, y_train, epochs=1200, batch_size=4):
+        """
+        this is the training function for
+        :param x_train:
+        :param y_train:
+        :return:
+        """
+        
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    if K.image_dim_ordering() == 'tf':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Convolution2D(nb_filter1, (1, 1), strides=strides, name=conv_name_base + '2a', trainable=trainable)(input_tensor)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter2, (kernel_size, kernel_size), padding='same', name=conv_name_base + '2b', trainable=trainable)(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter3, (1, 1), name=conv_name_base + '2c', trainable=trainable)(x)
+    x = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Convolution2D(nb_filter3, (1, 1), strides=strides, name=conv_name_base + '1', trainable=trainable)(input_tensor)
+    shortcut = FixedBatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = Add()([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block_td(input_tensor, kernel_size, filters, stage, block, input_shape, strides=(2, 2), trainable=True):
+
+    # conv block time distributed
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    if K.image_dim_ordering() == 'tf':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = TimeDistributed(Convolution2D(nb_filter1, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), input_shape=input_shape, name=conv_name_base + '2a')(input_tensor)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Convolution2D(nb_filter2, (kernel_size, kernel_size), padding='same', trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '2b')(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Convolution2D(nb_filter3, (1, 1), kernel_initializer='normal'), name=conv_name_base + '2c', trainable=trainable)(x)
+    x = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '2c')(x)
+
+    shortcut = TimeDistributed(Convolution2D(nb_filter3, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'), name=conv_name_base + '1')(input_tensor)
+    shortcut = TimeDistributed(FixedBatchNormalization(axis=bn_axis), name=bn_name_base + '1')(shortcut)
+
+    x = Add()([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+def nn_base(input_tensor=None, trainable=False):
+
+    # Determine proper input shape
+    if K.image_dim_ordering() == 'th':
+        input_shape = (3, None, None)
+    else:
+        input_shape = (None, None, 3)
+
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor, shape=input_shape)
+        else:
+            img_input = input_tensor
+
+    if K.image_dim_ordering() == 'tf':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+
+  
+	x=model
+        model = Model(inputs=[inputs], outputs=[conv10])
+        model.compile(optimizer=Adam(lr=1e-5), loss=UNET_Segmenter.dice_coef_loss, metrics=[UNET_Segmenter.dice_coef])
+        print(model.summary())
+
+        # training network
+        model.fit([x_train], [y_train], batch_size=batch_size, epochs=epochs, shuffle=True)
+
+        # set as class's model to be used for prediction
+        self.trained_model = model
+
+        return model
