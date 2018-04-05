9c3dcab5f54ce0a2230e35aa550c55cbeb955a31
Omid <os14526@uga.edu>
2018-04-05 10:37:30 -0400

    adds optical flow preprocessor initial implementation

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -68,7 +68,11 @@ class preprocessor:
             """
             if the source is not in the desired format change it to the desired size  
             """
-            temp_mask = np.zeros((target_dim[0], target_dim[1]))
+            if (len(dims) == 2):
+
+                temp_mask = np.zeros((target_dim[0], target_dim[1]))
+            else:
+                temp_mask = np.zeros((target_dim[0], target_dim[1],3))
 
             temp_mask[:dims[0], :dims[1]] = source
 
